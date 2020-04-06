1bde7a8d0ab5818ac5369ab4eb0e8a4f82e9d895
Peter Cnudde <peter.cnudde@gmail.com>
2020-04-06 14:26:09 -0700

    fix scoring

--- js/code.js
+++ js/code.js
@@ -5,10 +5,18 @@ function score() {
     items = $('#list1').getElementsByTagName("div")
     var i;
     correct = true
+    count = 1
     for (i = 0; i < items.length; i++) {
-        j = items[i].id.slice(1)-1
-        if (i!=j) {
-            correct = false
+        // only look at div items starting with 'i'
+        if (items[i].id.slice(0,1) == "i") {
+            j = items[i].id.slice(1)
+            console.log(items[i].id)
+
+            if (count!=j) {
+                console.log(i,j)
+                correct = false
+            }
+            count = count + 1
         }
     }
     console.log(correct)
